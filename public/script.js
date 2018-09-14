335104b1094d532d7bc6b02ec116f46207eb5893
Barkbox Loaner <barkboxloaner@Barkboxs-MacBook-Air.local>
2018-09-14 15:19:20 -0400

    added imgur api call

--- public/script.js
+++ public/script.js
@@ -6,14 +6,20 @@ new Vue ({
     total: 0,
     items: [],
     cart: [],
-    search: ''
+    newSearch: 'anime',
+    lastSearch: '',
+    loading: false
   },
   methods: {
     onSubmit: function() {
+      this.items = [];
+      this.loading = true;
       this.$http
-      .get('/search/'.concat(this.search))
+      .get('/search/'.concat(this.newSearch))
       .then(function(res){
+        this.lastSearch = this.newSearch
         this.items = res.data
+        this.loading = false;
       })
     },
     addItem: function(index) {
@@ -58,5 +64,8 @@ new Vue ({
     currency: function(price) {
       return '$'.concat(price.toFixed(2))
     }
+  },
+  mounted: function () {
+    this.onSubmit();
   }
 });
